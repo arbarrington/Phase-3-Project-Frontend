8f80c35c8b325fcf4c94119edbb7250f32abbca2
Jake <jakepucci26@gmail.com>
2022-10-02 14:04:00 -0600

    maybe added components

---
+++ my-app-frontend/src/components/DecisionList.js
@@ -0,0 +1,19 @@
+import React, {useState, useEffect} from "react";
+
+
+//this will have an ul of decisions to vote on
+
+export default function DecisionList(){
+
+
+    return(
+        <div>
+            <ul className="list-of-decisions">
+
+            </ul>
+        </div>
+
+
+    )
+    
+}
---
+++ my-app-frontend/src/components/FInalDecision.js
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from "react";
+
+
+// just defining class for css stuff
+
+
+export default function FinalDecision(){
+
+    return(
+<div classname="final-decision">
+
+</div>
+    )
+    
+}
