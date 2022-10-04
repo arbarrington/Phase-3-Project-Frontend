4336e67a2ec6a888cc6ae0984f5d574b00dcbd22
Jake <jakepucci26@gmail.com>
2022-10-04 13:51:48 -0600

    created a fetch's component

---
+++ my-app-frontend/src/components/Fetchs.js
@@ -0,0 +1,33 @@
+import React, {useState, useEffect} from "react";
+
+export default function Fetchs(){
+    //Reads
+    const readDecisions = 
+        useEffect(() => {
+            fetch('http;//localhost:9292/decisions')
+            .then((d) => d.json()
+            .then((d => (console.log(d)))))
+        }, [])
+
+    const readUsers = 
+    useEffect(() => {
+        fetch('http;//localhost:9292/users')
+        .then((d) => d.json()
+        .then((d => (console.log(d)))))
+    }, [])
+
+    const readJoints =
+    useEffect(() => {
+        fetch('http;//localhost:9292/joints')
+        .then((d) => d.json()
+        .then((d => (console.log(d)))))
+    }, [])
+
+    const readCompleted =
+    useEffect(() => {
+        fetch('http;//localhost:9292/completed')
+        .then((d) => d.json()
+        .then((d => (console.log(d)))))
+    }, [])
+
+}
