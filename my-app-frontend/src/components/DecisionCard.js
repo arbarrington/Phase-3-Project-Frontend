99eaf4c8bd2dba4ab96c002e51af3a252ec2ac88
AustinBarrington <arbarrington12@gmail.com>
2022-10-05 11:47:00 -0600

    rendering with no functionality

--- my-app-frontend/src/components/DecisionCard.js
+++ my-app-frontend/src/components/DecisionCard.js
@@ -5,15 +5,19 @@ export default function DecisionCard({options, decision, numOptions}) {
     const [hasChosenTwo, setHasChosenTwo] = useState(false)
     
 
-    function handleFirstChoice(e){
-        e.preventDefault()
+    function handleFirstChoice(){
+       
         console.log("first click")
         setHasChosenOne(true)
     }
-    function handleSecondChoice(e){
-        e.preventDefault()  
-        console.log("second click")
-        setHasChosenTwo(true)
+    function handleSecondChoice(){
+        if (hasChosenOne == false) {
+            console.log('awaiting first click')
+        } else if (hasChosenOne == true && hasChosenTwo == false)
+        {console.log("second click")
+        setHasChosenTwo(true)}
+        else
+        {console.log('two choices already made')}
     }
 
     function displayOptions(optionArray, arrayLength) {
@@ -29,7 +33,7 @@ export default function DecisionCard({options, decision, numOptions}) {
     return (
         <div className="decisionCard">
             <form>
-        <h2 key={decision.id}>Where should {decision.group_name} go for {decision.decision_type} on {decision.event_time}?</h2>
+        <h2 key={decision.id}>Where should {decision.group_name} go for {decision.event_type} on {decision.event_time}?</h2>
             <div className="options">
             <ul className="optionList">
                 { displayOptions(options, numOptions) }
