9f8a46127312bbd782665ea34adbe0ddd5edd074
Jake <jakepucci26@gmail.com>
2022-10-05 15:39:45 -0600

    sunday driving

--- my-app-frontend/src/components/DecisionList.js
+++ my-app-frontend/src/components/DecisionList.js
@@ -9,7 +9,7 @@ import {v4 as uuid} from "uuid";
 
 export default function DecisionList({username, groupname, matchingDecision}){
 
-    const [someStateBusniess, setSomeStateBusiness] = useState([])
+    const [userFullDecision, setUserFullDecision] = useState([])
     
     
     useEffect(() => {
@@ -24,57 +24,44 @@ export default function DecisionList({username, groupname, matchingDecision}){
                     }
                 })
             })
-            setSomeStateBusiness(thingsToSet)
+            setUserFullDecision(thingsToSet)
         })
         },[])
 
-    console.log("some state busines",someStateBusniess)
+    console.log("some state businet", userFullDecision)
 
 
 
 
 
 
-    return (
-        <div>
-            <ul className="list-of-decisions">
-                <p> thanks for logging in, buckagroo {username} of {groupname}</p>
-                <p> howdy from the ladn of decions lists</p>
-                <p> time for some fun and {matchingDecision}</p>
-                {someStateBusniess.map(element =>
-                    <DecisionCard
-                        key={uuid()}
-                        decisionID={element.id}
-                    />
-                )}
-            </ul>
-export default function DecisionList({username,groupname}){
+     
     // optionArray should be the state variable theseOptions
     let optionArray = ["option1", "option2", "option3", "option4", "option5"]
-    let decision = [{
-        id: 7,
-        event_type: "Outdoor activities",
-        decided: false,
-        group_name: "Shelby, Jake, and Austin",
-        event_time: "DateTime.new(2022, 10, 16, 7, 30)",
-        decision_deadline: "DateTime.new(2022, 10, 15, 23, 59"
-        },
-        {
-        id: 8,
-        event_type: "Happy Hour",
-        decided: false,
-        group_name: "Phase 3",
-        event_time: "DateTime.new(2022, 10, 28, 18, 00)",
-        decision_deadline: "DateTime.new(2022, 10, 15, 23, 59"
-            }]
+    // let decision = [{
+    //     id: 7,
+    //     event_type: "Outdoor activities",
+    //     decided: false,
+    //     group_name: "Shelby, Jake, and Austin",
+    //     event_time: "DateTime.new(2022, 10, 16, 7, 30)",
+    //     decision_deadline: "DateTime.new(2022, 10, 15, 23, 59"
+    //     },
+    //     {
+    //     id: 8,
+    //     event_type: "Happy Hour",
+    //     decided: false,
+    //     group_name: "Phase 3",
+    //     event_time: "DateTime.new(2022, 10, 28, 18, 00)",
+    //     decision_deadline: "DateTime.new(2022, 10, 15, 23, 59"
+    //         }]
 
   
     return(
         <div>
             {
-            decision.map((d) => {
+            userFullDecision.map((d) => {
                 
-               return( <DecisionCard key={decision.id} options={optionArray} decision={d}/> )
+               return( <DecisionCard key={uuid()} options={optionArray} decision={d}/> )
             })
             }
             <input id="submitdecision" type="submit" value="Submit!" />
