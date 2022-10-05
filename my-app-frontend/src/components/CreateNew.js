a1f7e72dc3c92ecab7d82177dd9381117c14d4f8
Jake <jakepucci26@gmail.com>
2022-10-05 14:10:16 -0600

    better

--- my-app-frontend/src/components/CreateNew.js
+++ my-app-frontend/src/components/CreateNew.js
@@ -73,19 +73,6 @@ const postedDecision = {
         })
    };   
  
-//  const data = { username: 'example' };
-
-// fetch('https://example.com/profile', {
-//   method: 'POST', // or 'PUT'
-//   headers: {
-//     'Content-Type': 'application/json',
-//   },
-//   body: JSON.stringify(data),
-// })
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log('Success:', data);
-//   })
 
 return (
 <React.Fragment>
