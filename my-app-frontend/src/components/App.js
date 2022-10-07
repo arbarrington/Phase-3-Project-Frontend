5c9f98a27c689e5d07ee6e94678cade5658869e2
Shelby Lee <slee337@uw.edu>
2022-10-06 19:19:07 -0600

    exsisting data fetches gucci as well as login

--- my-app-frontend/src/components/App.js
+++ my-app-frontend/src/components/App.js
@@ -1,57 +1,73 @@
 import CreateNew from './CreateNew';
-import FinalDecision from './FinalDecision';
-import ListOptions from './ListOptions';
 import DecisionList from './DecisionList';
 import NavBar from './NavBar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Fetchs from './Fetchs';
+import Login from './Login';
 
-import Home from './Home';
 
 import React, {useState, useEffect} from 'react';
 
 
 function App() {
 
-  // most recent user
-  const [currentUser, setCurrentUser ] = useState('')
+  // get all the exsisting decisions from the db
+  const [allDecs, setAllDecs] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:9292/decisions')
+        .then(d => d.json())
+        .then((d) => setAllDecs(d))
+  },[])
 
-  // state for determing if logged in or not
-  const [hasLoggedIn, setHasLoggedIn] = useState(false)
+  // get all the exsisting options from the db
+  const [allOpts, setAllOpts] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:9292/options')
+        .then(d => d.json())
+        .then((d) => setAllOpts(d))
+  },[])
 
-  // state for getting group name -> used to determine which decisions to render
-  const [currentGroupName, setCurrentGroupName] = useState('')
+  // get all the exsisting joints from the db
+  const [allJoints, setAllJoints] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:9292/joints')
+        .then(d => d.json())
+        .then((d) => setAllJoints(d))
+  },[])
 
-  // state for a new decision id that gets created
-  const [decId, setDecId] = useState()
+  // have the current groupname accessible by all components
+  const [currentGroup, setCurrentGroup] = useState('')
 
-  // cheat code to pass options around
-  const [createdOptions, setCreatedOptions] = useState([])
+  // state for if the user has logged in or not (so doesn't revert back to false in the login file)
+  // state variable for if logged in -> use as terenary to render whatever you want
+  // just used to render a login message now, can change to reroute if  you want
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
 
-  const [rerender, setReRender] = useState(false)
+  // console logs for testing
+  // console.log('all decisions:',allDecs)
+  // console.log('all options',allOpts)
+  // console.log('all joints',allJoints)
+  // console.log('current group:',currentGroup)
+
+
 
   return (
     <BrowserRouter>
-      {hasLoggedIn ? <NavBar /> : <div></div>}
+      <NavBar/>
       <Routes>
-       
-        <Route path="/" element=
-        {<Home 
-          onCurrentUser={(newUser)=>setCurrentUser(newUser)}
-          onCurrentGroupName={(newGroupName)=>setCurrentGroupName(newGroupName)}
-          onHasLoggedIn={() => setHasLoggedIn(true)}
-          hasLoggedIn={hasLoggedIn}
-          username={currentUser.username}
-          groupname={currentUser.groupname}
+        <Route path="/" element={
+          <Login
+            onCurrentGroup={(newGroup)=>setCurrentGroup(newGroup)}
+            onSetIsLoggedIn={()=>setIsLoggedIn(true)}
+            isLoggedIn={isLoggedIn}
           />
         }>
         </Route>
 
         <Route path="/new" element=
           {<CreateNew 
-            onGetDecisionId={(newDecId)=>setDecId(newDecId)}
-            onCreatedOptions={(newOptions)=>setCreatedOptions([...createdOptions, newOptions])}
-            onSetRerender={()=>setReRender(true)}
+            // onGetDecisionId={(newDecId)=>setDecId(newDecId)}
+            // onCreatedOptions={(newOptions)=>setCreatedOptions([...createdOptions, newOptions])}
+            // onSetRerender={()=>setReRender(true)}
             />
           }>
         </Route>
@@ -60,10 +76,8 @@ function App() {
           createdOptions={createdOptions}
           rerender={rerender}
         />}></Route> */}
-       
-        <Route path="/final" element={<FinalDecision />}></Route>
-       
-        <Route path="/list-opt" element={<ListOptions />}></Route>
+      
+      
       
       </Routes>
 
@@ -72,3 +86,36 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+  // // most recent user
+  // const [currentUser, setCurrentUser ] = useState('')
+
+  // // state for determing if logged in or not
+  // const [hasLoggedIn, setHasLoggedIn] = useState(false)
+
+  // // state for getting group name -> used to determine which decisions to render
+  // const [currentGroupName, setCurrentGroupName] = useState('')
+
+  // // state for a new decision id that gets created
+  // const [decId, setDecId] = useState()
+
+  // // cheat code to pass options around
+  // const [createdOptions, setCreatedOptions] = useState([])
+
+    // const [allJoints, setallJoints] = useState([{
+  //   decision_id: null,
+  //   group_name: ''
+
+  // {<Home 
+  //   // onCurrentUser={(newUser)=>setCurrentUser(newUser)}
+  //   // onCurrentGroupName={(newGroupName)=>setCurrentGroupName(newGroupName)}
+  //   // onHasLoggedIn={() => setHasLoggedIn(true)}
+  //   // hasLoggedIn={hasLoggedIn}
+  //   // username={currentUser.username}
+  //   // groupname={currentUser.groupname}
+  //   />
+  // }>
--- my-app-frontend/src/components/NavBar.js
+++ my-app-frontend/src/components/NavBar.js
@@ -2,22 +2,16 @@ import React, {useState, useEffect} from "react";
 import { Link } from "react-router-dom";
 
 
-
-// ! going to require a nav with many links... really gonna need a good double check to ensure im doing this correct !
-
-
 export default function NavBar(){
 
     return(
+
         <div className="nav-bar">
             <Link to="/new">Create New Decision Choice</Link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <Link to="/final">See Final Decision</Link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             <Link to="/dec-opts">Decision List</Link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <Link to="/list-opt">List Options</Link>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-            <Link to="/">Home</Link>
-
-
+            <Link to="/">Log In</Link>
         </div>
+
     )
 
     
