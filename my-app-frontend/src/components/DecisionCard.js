9e65f7d1bc654b072c98286a7df90206dea67548
Shelby Lee <slee337@uw.edu>
2022-10-07 00:02:44 -0600

    decion card as far as ill take it

--- my-app-frontend/src/components/DecisionCard.js
+++ my-app-frontend/src/components/DecisionCard.js
@@ -1,28 +1,43 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
+import {v4 as uuid} from "uuid";
 
-export default function DecisionCard({options, decision}) {
+export default function DecisionCard({options, decision, currentGroup}) {
 
-    console.log('options insdie card', options)
-    console.log('decs insdie card', decision)
+    // state to show only the options that belong to the specific dec that is currently being rendered
+    const [thisDecOpts, setThisDecOpts] = useState([])
+
+    useEffect(() => {
+        let goodOpts = []
+        options.forEach((opt) => {
+            if (opt.decision_id == decision.id){
+                goodOpts.push(opt)
+            }
+        })
+        setThisDecOpts(goodOpts)
+    },[])
+
+    // each optoin gets rendered as a button but I was too lazy to add any functinality to the buttons!
+    // but it might be easy to add the voting logic if each option is just a button wiht like a hanndle onClick fucntion to patch/post
 
     return (
         <div className="decisionCard">
+            {thisDecOpts.length > 0 ?
+                <div>
+                    <p>Vote on {decision.event_type} for {currentGroup} </p>
+                    {thisDecOpts.map((opt) => 
+                        <div key={uuid()}>
+                            <button>
+                                {/* maybe add some functionality to the button so they click here and then that posts this optins as chosen?? */}
+                                {opt.option_name}
+                            </button>
+                        </div>
+                    )}
+                </div>
+            :
+            <p>this decion doesn't have any options!</p>
+            }
         </div>
     );
 }
 
-
-        // <div className="decisionCard">
-        //     <form>
-        // <h2 key={decision.id}>Where should {decision.group_name} go for {decision.event_type} on {decision.event_time}?</h2>
-        //     <div className="options">
-        //     <ul className="optionList">
-        //         <label>
-        //         { displayOptions(options) }
-        //         </label>
-        //     </ul>
-        //     </div>
-        //     <br/>
-        //     </form>
-        // </div>
 
--- my-app-frontend/src/components/DecisionList.js
+++ my-app-frontend/src/components/DecisionList.js
@@ -6,20 +6,16 @@ import {v4 as uuid} from "uuid";
 // export default function DecisionList({allDecs, currentGroup, allOpts, allJoints}){
 export default function DecisionList({groupDecs, currentGroup, groupOpts }){
 
-console.log('decs inside declist', groupDecs)
-console.log('opts inside declist', groupOpts)
-console.log('group inside declist', currentGroup)
+// like the filtering for matching decions logic probs should live here and I tried to do that but state was making it a bitch so
+// i pushed it all up to app so that the state would be updated by the time it got here
 
-
-return(
+return (
     <div>
         {groupDecs.length > 0 ?
             <div>
                 {groupDecs.map((dec) => 
-                    <div>
-                        <p>render was triggered</p>
-                        <div key={uuid()}/>
-                        <DecisionCard options={groupOpts} decision={dec}/>
+                    <div key={uuid()}>
+                        <DecisionCard options={groupOpts} decision={dec} currentGroup={currentGroup}/>
                     </div>
                 )}
             </div>
@@ -30,62 +26,6 @@ return(
 )
 }
 
-
-
-
-
-    // // state that contains all of that groups decions
-    // const [groupDecs, setGroupDecs] = useState([])
-
-    // // state that contains all of that groups options (options for ALL DECS)
-    // const [groupOpts, setGroupOpts] = useState([])
-
-    // // states used just for proper use effect sequencing
-    // const [triggerSecondUE, setTriggerSecondUE] = useState(false)
-    // const [triggerThirdUE, setTriggerThirdUE] = useState(false)
-    // const [triggerRender, setTriggerRender] = useState(false)
-
-    // // looking through all of the joints to extract decion ids that correspond to the current group name
-    // let matchingDecIds =[]
-    // useEffect(() => {
-    //     allJoints.forEach((joint) => {
-    //         if (joint.group_name==currentGroup && !matchingDecIds.includes(joint.decision_id)) {
-    //             matchingDecIds.push(joint.decision_id)
-    //         }
-    //     })
-    //     setTriggerSecondUE(true)
-    // },[allJoints])
-
-    // // looking through all of the decions and filling out a state of group decs where the dec id and group name math (via joins table beta)
-    // let matchingDecFull = []
-    // useEffect(() => {
-    //     allDecs.forEach((dec) => {
-    //         matchingDecIds.forEach((matchingId) => {
-    //             if (dec.id == matchingId && !matchingDecFull.includes(dec)) {
-    //                 matchingDecFull.push(dec)
-    //             }
-    //         })
-    //     })
-    //     setGroupDecs(matchingDecFull)
-    //     setTriggerThirdUE(true)
-    // },[])
-
-    // // looking through all the options and extracting ones that have forein keys that match the current groups decions
-    // // this will extract the options for ALL of the matching decions
-    // // matching options will be passed to the render card which will filter out whcih ones to dsipaly!!
-    // let matchingOptions = []
-    // useEffect(() =>{
-    //     allOpts.forEach((opt) => {
-    //         groupDecs.forEach((dec) => {
-    //             if (opt.decision_id == dec.id) {
-    //                 matchingOptions.push(opt)
-    //             }
-    //         })
-    //     })
-    //     setGroupOpts(matchingOptions)
-    //     setTriggerRender(true)
-    //     //setTimeout(() => {setTriggerRender(true)}, 4000);
-    // },[])
-
-    // // setTimeout(() => {console.log('delayed group decs', groupDecs)}, 2000);
-    // // setTimeout(() => {console.log('delayed group opts', groupOpts)}, 2000);
+// console.log('decs inside declist', groupDecs)
+// console.log('opts inside declist', groupOpts)
+// console.log('group inside declist', currentGroup)
